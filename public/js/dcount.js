d8c086b1265477e9b55171a72a20a74d7efac15d
Pete Sheurpukdi <psheurpu@ucsd.edu>
2019-02-19 17:45:03 -0800

    play page loop work

--- public/js/dcount.js
+++ public/js/dcount.js
@@ -8,3 +8,23 @@ $(document).ready(function() {
         window.location.href = document.location.origin + "/index";
     });
 });
+
+//JAVASCRIPT FOR LOOP RANGE SLIDER
+$(document).ready(function() {
+    $("#slider").slider({
+        min: 0,
+        max: 100,
+        step: 1,
+        values: [10, 90],
+        slide: function(event, ui) {
+            for (var i = 0; i < ui.values.length; ++i) {
+                $("input.sliderValue[data-index=" + i + "]").val(ui.values[i]);
+            }
+        }
+    });
+
+    $("input.sliderValue").change(function() {
+        var $this = $(this);
+        $("#slider").slider("values", $this.data("index"), $this.val());
+    });
+});
