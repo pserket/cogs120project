9c0614fe9db7613f6ef5971598970484baab4576
Pete Sheurpukdi <psheurpu@ucsd.edu>
2019-02-19 21:02:30 -0800

    nouislider T_T

--- public/js/dcount.js
+++ public/js/dcount.js
@@ -9,25 +9,14 @@ $(document).ready(function() {
 
 //JAVASCRIPT FOR LOOP RANGE SLIDER
 
-var output = $('#output');
+var slider = document.getElementById('slider');
 
-$('#range-slider').noUiSlider({
-    start: [0, 100], 
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
     range: {
-        'min': [0],
-        'max': [100]
+        'min': 0,
+        'max': 100
     }
-}).on('slide', function(evt) {
-    var lowerHandle=false;
-    var upperHandle=false;
-    if($(evt.target).find('.noUi-handle-lower').hasClass("noUi-active")) lowerHandle=true;
-    if($(evt.target).find('.noUi-handle-upper').hasClass("noUi-active")) upperHandle=true;
-    // In this event handler, I want to see which of the two sliders is being moved
-    // Is there a property of the 'evt' parameter which would tell me this?
-    if(lowerHandle==true) console.log('>>> LOWER <<<')
-    if(upperHandle==true) console.log('>>> UPPER <<<')
-    
-    
-    output.html($(this).val().join(' - '));
 });
 });
