af9fa4152078f7cf0b60520fdfb777f2a14ad1fc
Pete Sheurpukdi <psheurpu@ucsd.edu>
2019-02-27 22:56:16 -0800

    fixed going from play to edit

--- public/js/dcount.js
+++ public/js/dcount.js
@@ -8,4 +8,9 @@ $(document).ready(function() {
         window.location.href = document.location.origin + "/index";
     });
 
+    $('#edit-btn').click(function(e) {
+        e.preventDefault();
+        window.location.href = document.location.origin + "/index";
+    });
+
 });
